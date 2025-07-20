cd39f3cd45fd1e946df5d460452967bdddc80ee3
Yanna <yannajask@gmail.com>
2025-07-20 14:26:45 -0400

    package.json: fix deployment to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: "export",
-    basePath: isProd ? "/halftitle.github.io" : "",
-    assetPrefix: isProd ? "/halftitle.github.io" : "",
     images: {
         unoptimized: true,
     },
