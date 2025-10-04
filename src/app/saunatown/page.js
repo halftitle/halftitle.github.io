0f0b2454c79d2b7de2a2cb7da9439bbf057f07f4
Yanna <yannajask@gmail.com>
2025-10-04 11:03:31 -0400

    add background image to saunatown

--- src/app/saunatown/page.js
+++ src/app/saunatown/page.js
@@ -29,54 +29,56 @@ const images = [
 export default function Saunatown() {
     return (
         <div className={styles.page}>
-            <main className={styles.main}>
-                <header className={styles.header}>
-                    <a href="/saunatown">
-                        <Image src="/saunatown/saunatown-logo-white.png" alt="saunatown zine" width={300} height={200} />
-                    </a>
-                </header>
-                <ImageCarousel images={images} style={{ fontFamily: "'Roboto Mono', monospace" }} navColor="#fff" />
-                <div className="credits">
-                    <p>Kyle Alaan-Nguyen</p>
-                    <p>Charlotte Alarie</p>
-                    <p>Frankie Alarie</p>
-                    <p><a href="mailto:8asrielxx@gmail.com">Asriel</a></p>
-                    <p><a href="https://instagram.com/foxgloves_333">Daniel Bezerra</a></p>
-                    <p><a href="https://instagram.com/chloecha_">Chloe Cha</a></p>
-                    <p>Miranda Chance</p>
-                    <p><a href="https://www.instagram.com/rubylfaris/">Ruby Faris</a></p>
-                    <p><a href="https://instagram.com/taylordfranks">Taylor Franks</a></p>
-                    <p><a href="https://thomasfrenette.bandcamp.com">Thomas Frenette</a></p>
-                    <p><a href="https://yannajask.com">Yanna Jaskielewicz</a></p>
-                    <p>Kasen Korstanje</p>
-                    <p><a href="https://instagram.com/yem_ly">Zoe Lee</a></p>
-                    <p>Sylvie Liss</p>
-                    <p>Kyra Meier</p>
-                    <p>Drew Newell</p>
-                    <p>H.A. Porfiri</p>
-                    <p><a href="https://instagram.com/x.lm.nl">Zoe Sexton</a></p>
-                    <p>Aria Van Allen</p>
-                    <p><a href="https://instagram.com/siporawest">Sipora West</a></p>
-                    <p><a href="https://instagram.com/m4r94ux">Margaux Wilkinson</a></p>
-                    <p><a href="https://www.instagram.com/dianayamadaaa/">Diana Yamada</a></p>
-                </div>
-            </main>
-            <footer>
-                <div className="access">
-                    <a href="https://drive.google.com/drive/folders/1b3YTUG6MS25TIIBZg-2ySjX6pD1wZwvF?usp=drive_link">
-                        Access in Google Drive
-                    </a>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe1OOl0-pn-BtKuy05QPB89fd7qHmrIoZebChHImzRbrB6Kw/viewform">
-                        Order a physical copy
-                    </a>
-                </div>
-                <p>Summer 2025</p>
-                <div className="return">
-                    <a href="/">
-                        {'< Back'}
-                    </a>
-                </div>
-            </footer>
+            <div className="pageContainer">
+                <main className={styles.main}>
+                    <header className={styles.header}>
+                        <a href="/saunatown">
+                            <Image src="/saunatown/saunatown-logo-white.png" alt="saunatown zine" width={300} height={200} />
+                        </a>
+                    </header>
+                    <ImageCarousel images={images} style={{ fontFamily: "'Roboto Mono', monospace" }} navColor="#fff" />
+                    <div className="credits">
+                        <p>Kyle Alaan-Nguyen</p>
+                        <p>Charlotte Alarie</p>
+                        <p>Frankie Alarie</p>
+                        <p><a href="mailto:8asrielxx@gmail.com">Asriel</a></p>
+                        <p><a href="https://instagram.com/foxgloves_333">Daniel Bezerra</a></p>
+                        <p><a href="https://instagram.com/chloecha_">Chloe Cha</a></p>
+                        <p>Miranda Chance</p>
+                        <p><a href="https://www.instagram.com/rubylfaris/">Ruby Faris</a></p>
+                        <p><a href="https://instagram.com/taylordfranks">Taylor Franks</a></p>
+                        <p><a href="https://thomasfrenette.bandcamp.com">Thomas Frenette</a></p>
+                        <p><a href="https://yannajask.com">Yanna Jaskielewicz</a></p>
+                        <p>Kasen Korstanje</p>
+                        <p><a href="https://instagram.com/yem_ly">Zoe Lee</a></p>
+                        <p>Sylvie Liss</p>
+                        <p>Kyra Meier</p>
+                        <p>Drew Newell</p>
+                        <p>H.A. Porfiri</p>
+                        <p><a href="https://instagram.com/x.lm.nl">Zoe Sexton</a></p>
+                        <p>Aria Van Allen</p>
+                        <p><a href="https://instagram.com/siporawest">Sipora West</a></p>
+                        <p><a href="https://instagram.com/m4r94ux">Margaux Wilkinson</a></p>
+                        <p><a href="https://www.instagram.com/dianayamadaaa/">Diana Yamada</a></p>
+                    </div>
+                </main>
+                <footer>
+                    <div className="access">
+                        <a href="https://drive.google.com/drive/folders/1b3YTUG6MS25TIIBZg-2ySjX6pD1wZwvF?usp=drive_link">
+                            ACCESS IN GOOGLE DRIVE
+                        </a>
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfe1OOl0-pn-BtKuy05QPB89fd7qHmrIoZebChHImzRbrB6Kw/viewform">
+                            ORDER A PHYSICAL COPY
+                        </a>
+                    </div>
+                    <p>Summer 2025</p>
+                    <div className="return">
+                        <a href="/">
+                            {'< BACK'}
+                        </a>
+                    </div>
+                </footer>
+            </div>
         </div>
     );
 }
